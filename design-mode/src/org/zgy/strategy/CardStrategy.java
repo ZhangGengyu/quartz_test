e9f06bc9b2f143fa62884a5be4076b37a908fa3a
zhanggengyu <zhanggengyu@jsonmedia.com>
2020-12-23 16:16:46 +0800

    测试分支4

---
+++ design-mode/src/org/zgy/strategy/CardStrategy.java
@@ -0,0 +1,8 @@
+package org.zgy.strategy;
+
+public class CardStrategy implements IStrategy {
+    @Override
+    public String calculate(Integer playType) {
+        return "----card strategy----";
+    }
+}
